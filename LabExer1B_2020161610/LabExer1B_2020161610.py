2797747330cabe2e4a08469b16930b56791c2d1d
JvMapote <jayveenmapote@gmail.com>
2022-04-16 00:04:13 +0800

    commit

--- LabExer1B_2020161610/LabExer1B_2020161610.py
+++ LabExer1B_2020161610/LabExer1B_2020161610.py
@@ -35,14 +35,12 @@ class singly_linked_list:
             val = current_item.data
             current_item = current_item.next
             yield val
-
 items = singly_linked_list()
 items.append_item('PHP')
 items.append_item('Python')
 items.append_item('C#')
 items.append_item('C++')
 items.append_item('Java')
-
 print("Original list:")
 for val in items.iterate_item():
     print(val)
